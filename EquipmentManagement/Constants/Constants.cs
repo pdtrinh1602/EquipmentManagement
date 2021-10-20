6e027cac6a8304feb8535792e6c1c4b7b5b03d52
DaoDaoDaoDaoDaoDao <thudao10798@gmail.com>
2021-10-20 16:09:24 +0700

    Add Equipment controller

--- EquipmentManagement/Constants/Constants.cs
+++ EquipmentManagement/Constants/Constants.cs
@@ -3,6 +3,8 @@
     public class Constant
     {
         public const string FAIL_CREATE_USER = "Error creating new user";
+        public const string FAIL_CREATE_EQUIPMENT = "Error creating new equipment";
+        public const string DELETE_SUCCESS = "Delete Success";
     }
     
 }
---
+++ EquipmentManagement/Repository/IEquipmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentModel = Equipment.Models.Equipment;
+
+namespace EquipmentManagement.Repository
+{
+    public interface IEquipmentRepository
+    {
+        IEnumerable<EquipmentModel> GetEquipment();
+        EquipmentModel GetEquipmentById(int id);
+        List<EquipmentModel> SearchEquipment(string value);
+        void CreateEquipmnet(EquipmentModel equipment);
+        void DeleteEquipmnet(int id);
+        void Save();
+
+    }
+}
